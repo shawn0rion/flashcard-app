63a5f20e476e8ca49127216e3a8d56ad87ccef55
shawn0rion <shawnorion07@gmail.com>
2023-06-12 21:14:48 -0500

    creates test deck with functional quiz

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+// define elements
+const container = document.querySelector('.container');
+const viewDecksBtn = document.querySelector('#view-decks')
+const addCardBtn = document.querySelector('#add-card');
+const addCardForm = document.querySelector('#card-form');
+const addDeckBtn = document.querySelector('#add-deck');
+const addDeckForm = document.querySelector('#deck-form')
+const homeContainer = document.querySelector('.home-container');
+const quizContainer = document.querySelector('.quiz-container');
+const decksContainer = document.querySelector('.decks-container');
+
+// define variables
+const decks = [];
+
+// define functions which create objects
+function createDeck(title = ''){
+    if (title == ''){
+        console.error('unnamed deck will not be created.')
+        return;
+    }
+    const deck = new Deck(title);
+    decks.push(deck);
+    return deck;
+}
+
+//define classes
 class Deck {
-    constructor(){
+    constructor(title){
+        this.title = title;
         this.cards = [];
     }
-    newFlashcard(question, answer){
+    addCard(question, answer){
         if (question !== '' && answer !== ''){
-            const card = new Flashcard(question, answer, this);
-            this.addCard(card);
+            const card = new Card(question, answer, this);
+            this.cards.push(card);
             return card;
         }
     }
 
-    addCard(card){
-        this.cards.push(card);
-    }
     removeCard(card){
         const idx = this.cards.indexOf(card);
         if (idx + 1 === this.cards.length){
@@ -21,17 +45,31 @@ class Deck {
             this.cards = this.cards.slice(0,idx).concat(this.cards.slice(idx + 1))
         }
     }
+
+    selectRandomQuestion(){
+        const idx = Math.floor(Math.random() * this.cards.length);
+        return this.cards[idx];
+    }
+    startQuiz(){
+        console.log('starting quiz: ', this)
+        if (this.cards.length < 1) return;
+        console.log('for real')
+        homeContainer.classList.add('hide');
+        quizContainer.classList.remove('hide');
+        renderFooter();
+        handleQnaEvents(this);
+        renderQna(this);
+
+    }
 }
 
-class Flashcard {
+class Card {
     constructor(question, answer, deck){
         this.question = question;
         this.answer = answer;
         this.deck = deck;
     }
-    // could edit deck in future too
     edit(question, answer){
-        // if there is no change, there is no problem
         this.question = question;
         this.answer = answer;
     }
@@ -40,65 +78,150 @@ class Flashcard {
     }
 }
 
+// define render functions
 
+function renderQna(deck){
+    const question = document.querySelector('.question');
+    const answer = document.querySelector('.answer');
 
-const deck = new Deck();
-const container = document.querySelector('.container');
-const newFlashcardBtn = document.querySelector('.new-flashcard-btn');
-const newFlashcardForm = document.querySelector('.new-flashcard-form');
-
-// handle new card events
-newFlashcardBtn.addEventListener('click', event => {
-    newFlashcardForm.classList.toggle('hide')
-    document.body.style.overflowY = 'hidden';
-})
-const closeFormBtn = document.querySelector('.close-form-btn');
-closeFormBtn.addEventListener('click', event => {
-    newFlashcardForm.classList.add('hide');
-    container.classList.remove('show-form');
-    document.body.style.overflowY = 'scroll';
-})
-newFlashcardForm.addEventListener('submit', event => {
-    event.preventDefault();
-    // hide form
-    newFlashcardForm.classList.add('hide');
-    document.body.style.overflowY = 'scroll';
-
-    const question = event.target.querySelector('#question-input').value;
-    const answer = event.target.querySelector('#answer-input').value;
-    const card = deck.newFlashcard(question, answer);
-    console.log(card);
-    if (card !== undefined){
-        renderCard(card);
+    let card = deck.selectRandomQuestion();
+    // get a new card
+    while(card.question == question.textContent &&
+        card.answer == answer.textContent){
+        card = deck.selectRandomQuestion();
     }
-})
 
-function renderCard(cardObject){
-    const deckElement = document.querySelector('.deck');
-    const cardElement = document.createElement('div');
-    cardElement.className = 'card';
-    const question = document.createElement('div');
-    question.className = 'question';
-    question.textContent = cardObject.question;
-    const answer = document.createElement('div');
-    answer.className = 'answer';
-    answer.textContent = cardObject.answer;
-    const showHideBtn = document.createElement('button');
-    showHideBtn.className = 'show-hide-btn';
-    showHideBtn.textContent = 'Show / Hide';
-    showHideBtn.addEventListener('click', event => {
-        if (cardElement.classList.contains('show')){
-            cardElement.classList.remove('show');
-        } else{
-            cardElement.classList.add('show');
-        }
-    })
-    // add edit
-    // add delete
-    cardElement.appendChild(question);
-    cardElement.appendChild(showHideBtn);
-    cardElement.appendChild(answer);
-    deckElement.appendChild(cardElement);
+    console.log('current card: ', card)
+    // display card values
+    question.textContent = card.question;
+    answer.textContent = card.answer;
+    // hide answer
 }
 
-renderCard(deck.newFlashcard('james jean book', 'pareidolia'));
+function renderDeck(deck){
+    console.log('rendering deck')
+    const decksEl = document.querySelector('.decks')
+    const deckEl = document.createElement('div');
+    const title = document.createElement('button')
+
+    deckEl.className = 'deck';
+    title.className = 'title';
+
+    title.textContent = deck.title;
+    // if title is clicked, then render quizContainer 
+    title.addEventListener('click', event => {
+        event.target.blur()
+        deck.startQuiz();
+    })
+    deckEl.appendChild(title);
+    decksEl.appendChild(deckEl);
+}
+
+// to do: add event for edit Card 
+function renderFooter(){
+    const footer = document.querySelector('footer');
+    footer.innerHTML = '';
+    if (homeContainer.classList.contains('hide')){
+        footer.style.justifyContent = 'space-between'
+
+        const editBtn = document.createElement('button');
+        const showOrNextBtn = document.createElement('button');
+        const emptyDiv = document.createElement('div');
+
+        editBtn.id = 'edit-card';
+        showOrNextBtn.id = 'show-or-next';
+        emptyDiv.className ='placeholder';
+
+        editBtn.textContent = 'Edit';
+        showOrNextBtn.textContent = 'Show Answer'
+
+        // add listener to edit button, such that editCardForm pops up
+
+        footer.appendChild(editBtn)
+        footer.appendChild(showOrNextBtn)
+        footer.appendChild(emptyDiv)
+
+    } else{
+        footer.style.justifyContent = 'center';
+        const addDeckBtn = document.createElement('button')
+        addDeckBtn.id = 'add-deck';
+        addDeckBtn.textContent = 'Create Deck'
+        addDeckBtn.addEventListener('click', event => {
+            addDeckForm.classList.remove('hide');
+        })
+        footer.appendChild(addDeckBtn);
+    }
+}
+
+// handle events
+
+
+function handleQnaEvents(deck){
+    const qnaContainer = document.querySelector('.qna');
+    const showOrNextBtn = document.querySelector('#show-or-next');
+    if (qnaContainer.classList.contains('show')) qnaContainer.classList.remove('show');
+
+    // handle click events
+    showOrNextBtn.textContent = 'Show Answer'
+    showOrNextBtn.addEventListener('click', event => {
+        if (!qnaContainer.classList.contains('show')){
+            qnaContainer.classList.add('show');
+            event.currentTarget.textContent = 'Next Question'
+        } else{
+            if (qnaContainer.classList.contains('show')) qnaContainer.classList.remove('show');
+            renderQna(deck);
+        }}
+    );
+
+}
+
+viewDecksBtn.addEventListener('click', event => {
+    homeContainer.classList.remove('hide');
+    quizContainer.classList.add('hide');
+    renderFooter();
+})
+const closeFormBtns = document.querySelectorAll('.close-form');
+
+closeFormBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        let form = event.target.parentElement;
+        while(form.tagName !== 'FORM' ){
+            form = event.target.parentElement
+        }
+        form.classList.add('hide');
+        console.log('hidden form:', form)
+    })
+})
+
+addCardBtn.addEventListener('click', event => {
+    addCardForm.classList.remove('hide');
+})
+
+addCardForm.addEventListener('submit', event => {
+    event.preventDefault();
+    // hide form
+    addCardForm.classList.add('hide');
+})
+addDeckForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const title = document.querySelector('#deck-name').value;
+    const deck = createDeck(title);
+    // deck is undefined if title is empty string
+    if (deck == undefined) return;
+
+    renderDeck(deck);
+    event.target.classList.add('hide');
+})
+
+// init with quiz-container hidden
+renderFooter();
+
+// test 
+const testDeck = createDeck('test');
+testDeck.addCard("Why did the chicken cross the road?", "To get to the other side.")
+testDeck.addCard("What did one wall say to the other wall?", "I'll meet you at the corner!")
+testDeck.addCard("Why don't scientists trust atoms?", "Because they make up everything!")
+testDeck.addCard("Why couldn't the bicycle stand up by itself?", "It was two-tired!")
+testDeck.addCard("Why did the tomato turn red?", "Because it saw the salad dressing!")
+renderDeck(testDeck);
+testDeck.startQuiz();
